f1319c65f6661ed21c74e25c017c4244380bb422
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2023-02-25 19:00:02 +0100

    add staging imgw models

---
+++ dbtlastfm/models/imgw/stg_imgw__stations.sql
@@ -0,0 +1,13 @@
+select
+    "full_code",
+    "name",
+    "short_code",
+from
+    read_csv(
+        "../rawdata/imgw/stations/*.csv",
+        columns = {
+            'full_code': 'varchar',
+            'name': 'varchar',
+            'short_code': 'varchar'
+        }
+    )
